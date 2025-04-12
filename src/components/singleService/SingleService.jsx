08ca25077f7230bf36666854193df5a81126298f
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-04-13 00:01:23 +0200

    Update SingleService.jsx

--- src/components/singleService/SingleService.jsx
+++ src/components/singleService/SingleService.jsx
@@ -9,7 +9,6 @@ import BlurFade from '../ui/blur-fade'
 import img1 from '/public/images/detservices/2.jpg'
 import img3 from "/public/images/detservices/3.jpg"
 import img4 from "/public/images/detservices/4.jpg"
-import img5 from "/public/images/detservices/5.jpg"
 import img6 from "/public/images/detservices/6.jpg"
 import img7 from "/public/images/detservices/7.jpg"
 import img8 from "/public/images/detservices/8.jpg"
@@ -67,7 +66,7 @@ export default function SingleService() {
         ],
        cover : img1,
         images: [
-            img1, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19
+            img1, img3, img4, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19
         ]
     }  
 
